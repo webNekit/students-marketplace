215d85b8f9b2cc0a9cd5f78ef3e1a190bf8033da
Nikita-Hunter <86223001+Nikita-Hunter@users.noreply.github.com>
2024-06-11 12:11:07 +0300

    Исправил ошибки

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -10,9 +10,6 @@ export function Navbar() {
     return (
         <nav className="w-full">
             <menu className="w-full flex flex-wrap md:flex-nowrap items-center justify-center gap-2 md:gap-5">
-                <li className="inline-flex">
-                    <Link href={"/"} className="p-2 text-gray-400 transition-colors rounded-md hover:text-white hover:bg-white/5">Главная</Link>
-                </li>
                 {Links.map((link, index) => {
                     return (
                         <li key={index} className="inline-flex">
